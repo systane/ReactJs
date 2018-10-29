af503ef9fd63d7fc0daf88625f433696416e7370
Luis Fernando <lsilva49@csc.com>
2018-10-29 17:45:26 -0300

    Add dynamic behavior to fill the author's table

--- src/App.js
+++ src/App.js
@@ -3,6 +3,32 @@ import './css/pure-min.css';
 import './css/side-menu.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {authorList: []};
+  }
+
+
+  /* 
+  lifecycle methods:
+    componentWillMount()   --> É invocada antes do componente ser renderizado no DOM pela primeira vez
+    componentDidMount()    --> É invocada logo após o componente ser renderizado no DOM pela primeira vez --> Normalmente utilizado para requisições assincronas, pois permitir primeiro renderizar o html e somente após atualizar o html com a resposta de alguma request realizada.
+    componentWillUnmount() --> É invocada logo após o componente ser desmontado - removido - do DOM
+  */
+
+ componentDidMount(){
+    fetch("http://localhost:8888/api/autores")
+      .then(res => res.json()) /*.then(function(response) { return response.json(); }) res.json() --> retorna o body da response "parseado" como JSON*/
+      .then((result) => {
+        this.setState({authorList: result});
+      },
+      (error) => {
+        console.log(error);
+      }
+    )
+  }
+
   render() {
     return (
       <div id="layout">
@@ -10,18 +36,15 @@ class App extends Component {
           <span></span>
         </a>
 
+        {/*Side-menu*/}
         <div id="menu">
           <div className="pure-menu">
             <a className="pure-menu-heading" href="#">Company</a>
 
             <ul className="pure-menu-list">
               <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-              <li className="pure-menu-item"><a href="#" className="pure-menu-link">About</a></li>
-
-              <li className="pure-menu-item menu-item-divided pure-menu-selected">
-                <a href="#" className="pure-menu-link">Services</a>
-              </li>
-
+              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Author</a></li>
+              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Books</a></li>
               <li className="pure-menu-item"><a href="#" className="pure-menu-link">Contact</a></li>
             </ul>
           </div>
@@ -29,37 +52,55 @@ class App extends Component {
 
         <div id="main">
           <div className="header">
-            <h1>Page Title</h1>
-            <h2>A subtitle for your page goes here</h2>
+            <h1>Author</h1>
+          </div>
+        </div>
+
+        {/*Form*/}
+        <div className="content" id="content">
+          <div className="pure-form pure-form-aligned">
+            <form className="pure-form pure-form-aligned">
+              <div className="pure-control-group">
+                <label htmlFor="name">Name</label>
+                <input id="name" type="text" name="name" />
+              </div>
+              <div className="pure-control-group">
+                <label htmlFor="email">Email</label>
+                <input id="email" type="email" name="email" />
+              </div>
+              <div className="pure-control-group">
+                <label htmlFor="password">Password</label>
+                <input id="password" type="password" name="password" />
+              </div>
+              <div className="pure-control-group">
+                <label></label>
+                <button type="submit" className="pure-button pure-button-primary">Submit</button>
+              </div>
+            </form>
           </div>
 
-          <div className="content">
-            <h2 className="content-subhead">How to use this layout</h2>
-            <p>
-              To use this layout, you can just copy paste the HTML, along with the CSS in <a href="/css/layouts/side-menu.css" alt="Side Menu CSS">side-menu.css</a>, and the JavaScript in <a href="/js/ui.js">ui.js</a>. The JS file uses vanilla JavaScript to simply toggle an <code>active</code> class that makes the menu responsive.
-            </p>
-
-            <h2 className="content-subhead">Now Let's Speak Some Latin</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-
-            <div className="pure-g">
-              <div className="pure-u-1-4">
-                <img className="pure-img-responsive" src="http://farm3.staticflickr.com/2875/9069037713_1752f5daeb.jpg" alt="Peyto Lake" />
-              </div>
-              <div className="pure-u-1-4">
-                <img className="pure-img-responsive" src="http://farm3.staticflickr.com/2813/9069585985_80da8db54f.jpg" alt="Train" />
-              </div>
-              <div className="pure-u-1-4">
-                <img className="pure-img-responsive" src="http://farm6.staticflickr.com/5456/9121446012_c1640e42d0.jpg" alt="T-Shirt Store" />
-              </div>
-              <div className="pure-u-1-4">
-                <img className="pure-img-responsive" src="http://farm8.staticflickr.com/7357/9086701425_fda3024927.jpg" alt="Mountain" />
-              </div>
-            </div>
-            <h2 className="content-subhead">Try Resizing your Browser</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          {/*Records*/}
+          <div>
+            <table className="pure-table">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>email</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  this.state.authorList.map(function (author) {
+                    return (
+                      <tr key={author.id}>
+                        <td>{author.nome}</td>
+                        <td>{author.email}</td>
+                      </tr>
+                    );
+                  })
+                }
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
