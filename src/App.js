1d7f8fd704090018841a49f1f9ab1195ad73c9e3
Luis Fernando <lf.silva@outlook.com>
2018-11-01 07:57:19 -0300

    Added a new component(FormInput) to receive input from forms

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
+import FormInput from './components/FormInput';
 
 class App extends Component {
 
@@ -30,7 +31,7 @@ class App extends Component {
  componentDidMount(){
     fetch("http://localhost:8080/api/autores")
       .then(res => res.json()) /*.then(function(response) { return response.json(); }) res.json() --> retorna o body da response "parseado" como JSON*/
-      .then((result) => {
+      .then(result => {
         this.setState({authorList: result});
       },
       (error) => {
@@ -53,7 +54,6 @@ class App extends Component {
     })
     .then(res => res.json())
     .then((updatedAuthorList) => {
-      console.log(this);
       this.setState({authorList: updatedAuthorList});
     }, (error) => {
       console.log({failedToSaveAuthor: error});
@@ -104,18 +104,9 @@ class App extends Component {
         <div className="content" id="content">
           <div className="pure-form pure-form-aligned">
             <form className="pure-form pure-form-aligned" onSubmit={this.submitForm} method="post">
-              <div className="pure-control-group">
-                <label htmlFor="name">Name</label>
-                <input id="name" type="text" name="name" value={this.state.name} onChange={this.setName}/>
-              </div>
-              <div className="pure-control-group">
-                <label htmlFor="email">Email</label>
-                <input id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail}/>
-              </div>
-              <div className="pure-control-group">
-                <label htmlFor="password">Password</label>
-                <input id="password" type="password" name="password" value={this.state.password} onChange={this.setPassword}/>
-              </div>
+              <FormInput id="name" type="text" name="name" value={this.state.name} onChange={this.setName} label="Name"/>
+              <FormInput id="email" type="text" name="email" value={this.state.email} onChange={this.setEmail} label="Email"/>
+              <FormInput id="password" type="text" name="password" value={this.state.password} onChange={this.setPassword} label="Password"/>
               <div className="pure-control-group">
                 <label></label>
                 <button type="submit" className="pure-button pure-button-primary">Submit</button>
