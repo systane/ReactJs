2f281215992488bc52d84464f6d0ea27915cbbcd
Luis Fernando <lf.silva@outlook.com>
2018-11-02 10:55:17 -0300

    Added a new component to encapsulate the Side Menu

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './css/pure-min.css';
 import './css/side-menu.css';
 import FormInput from './components/FormInput';
 import FormSubmitButton from './components/FormSubmitButton';
+import SideMenu from './components/SideMenu';
 
 class App extends Component {
 
@@ -82,18 +83,7 @@ class App extends Component {
         </a>
 
         {/*Side-menu*/}
-        <div id="menu">
-          <div className="pure-menu">
-            <a className="pure-menu-heading" href="#">Company</a>
-
-            <ul className="pure-menu-list">
-              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Author</a></li>
-              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Books</a></li>
-              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Contact</a></li>
-            </ul>
-          </div>
-        </div>
+        <SideMenu/>
 
         <div id="main">
           <div className="header">
---
+++ src/components/SideMenu.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+export default class SideMenu extends Component {
+    render() {
+        return (
+            <div id="menu">
+                <div className="pure-menu">
+                    <a className="pure-menu-heading" href="#">Company</a>
+
+                    <ul className="pure-menu-list">
+                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
+                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">Author</a></li>
+                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">Books</a></li>
+                        <li className="pure-menu-item"><a href="#" className="pure-menu-link">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
